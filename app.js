453ea163a530d9b19cac7987280c218f63d3e38f
Karen Geerts <karen.geerts@gmail.com>
2018-02-13 14:41:20 +1100

    set color when using enter functionality working

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ $( ".set_color_button" ).click(function() {
   value = $(".colourpicker").val();
   $(".brushbox").css("background-color", value);
 });
+
+$(".colourpicker").keypress(function(e) {
+    if(e.which == 13) {
+      value = $(".colourpicker").val();
+      $(".brushbox").css("background-color", value);
+    }
+});
