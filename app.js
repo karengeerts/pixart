44d2cb49f6016651c36b841c75616dd8e5812e2b
Karen Geerts <karen.geerts@gmail.com>
2018-02-13 15:02:46 +1100

    added 100 square divs to body

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var $body = $('.body');
 
 $( ".set_color_button" ).click(function() {
   value = $(".colourpicker").val();
@@ -9,4 +10,15 @@ $(".colourpicker").keypress(function(e) {
       value = $(".colourpicker").val();
       $(".brushbox").css("background-color", value);
     }
+});
+
+var addSquares = function(){
+  for (var i = 0; i < 100; i++){
+    var $newSquare = $('<div>');
+    $("body").append($newSquare);
+  }
+};
+
+$(document).ready(function () {
+    addSquares();
 });
